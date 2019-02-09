e5076c657a3b9456538be167d691327819333cf4
graciaLabNico <developer.software@gracialab.co>
2019-02-09 13:34:22 -0500

    Update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('UPDATE')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
