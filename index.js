6cc262bb10a5dbf7438cfe0401072d3698fe5b30
nicol <nicol@SALAZARNICOLAS>
2019-02-09 13:09:14 -0500

    UPDATE NICO

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-  });
-
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+console.log('UPDATE')
