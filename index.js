8de4785e28e26e1364903dcf5aa6635826f226ca
juanpablo144 <juanpabloperez990215@gmail.com>
2019-02-16 12:12:50 -0500

    update

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+// importamos libreria Express
 const express = require('express');
+// importamos libreria Path
 const path = require('path');
 
 const app = express();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+// se coloca una ruta estatica para los archivos del proyecto
+app.use(express.static(path.join(__dirname, '/assets/pages')));
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/pages/index.html'));
+// creamos una funcion de una ruta get
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+app.get('/prueba', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/assets/pages/prueba.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+// Inicializamos el app en el puerto 3000
 app.listen(port);
 
+// en consola imprimimos por que puerto esta corriendo
 console.log('App is listening on port ' + port);
